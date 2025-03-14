eced15ca7c7c3f945204a034e2bb9e0489dc7e65
Ganesh <shidganesh0297@gmail.com>
2025-03-14 22:30:01 +0530

    First Commit

---
+++ GitHub_Practice_Project/TestProgram.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GitHub_Practice_Project
+{
+    internal class TestProgram
+    {
+        public void show()
+        {
+            Console.WriteLine("Hello this is Ganesh");
+        }
+    }
+}
